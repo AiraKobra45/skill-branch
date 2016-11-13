adbeda7ae833422c962abf04339b91d835ad72cf
AiraKobra45 <AiraKobra45@yandex.ru>
2016-11-13 10:52:19 +0900

    1234

---
+++ src/canonize.js
@@ -0,0 +1,12 @@
+export default function canonize (url){
+  // TODO custom domain name
+  const re = new RegExp('@?(https?:)?(\/\/)?((\S*)[^\/]*\/)?([@_a-z0-9.]*)', 'i');
+  var mat = url.match(re);
+  console.log(mat);
+  const username = mat[5];
+  if ( username.substring(0, 1) == '@' ) {
+    return username;
+  } else {
+    return '@' + username;
+  };
+};
